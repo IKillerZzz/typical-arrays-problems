c6a9d06aa77c2255d70bc04a935f40a994134911
Ilya Pol <aliveshooter@gmail.com>
2020-09-29 21:24:02 +0300

    feat: solve typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = min = arr => arr && arr.length !== 0 ? Math.min.apply(null, arr) : 0;
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = max = arr => arr && arr.length !== 0 ? Math.max.apply(null, arr) : 0;
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = avg = arr => arr && arr.length !== 0 ? arr.reduce((sum, i) => sum + i, 0) / arr.length : 0;
